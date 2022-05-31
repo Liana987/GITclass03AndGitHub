f7abb7e8dbc65d1e3e88d506fd1be687f16b1da8
Liana <horvatliana87@gmail.com>
2022-05-31 18:54:26 -0500

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -1,5 +1,16 @@
 package github;
 
 public class gitAndGitHub {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Today we will learn github");
+		/*Tracking our project in git.
+		 * 1.Navigate our terminal to the location where we want to create the repository
+		 * 2. initialize the repository (git init ; git status; git add . ; git commit ; git log; )
+		 */
+		System.out.println("Today is a very good day");
+		//adding additional code- will be red
+	}
 
 }
